935aeb70e31eada7c2fddf432c68fcf71eba12eb
Vitaliy-Karpenko529 <vkarpenko945@gmail.com>
2022-01-22 20:29:14 +0200

    fix all bugs2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,78 @@
+// import React, { useState } from 'react';
+// import { nanoid } from 'nanoid';
+// import 'modern-normalize/modern-normalize.css';
+// import Container from './components/Container/Container';
+// import ContactsForm from './components/ContactsForm/ContactsForm';
+// import ContactsList from './components/ContactsList/ContactsList';
+// import Filter from './components/Filter/Filter';
+// import { useLocaleStorage } from '../src/hooks/useLocaleStorage';
+
+// const Title = ['Title'];
+
+// export default function App() {
+//   const [filter, setFilter] = useState('');
+//   const [contacts, setContacts] = useLocaleStorage('contacts', []);
+
+//   const addContact = ({ name, number }) => {
+//     const newContact = {
+//       id: nanoid(),
+//       name,
+//       number,
+//     };
+
+//   //   if (
+//   //     contacts.find(
+//   //       contact => contact.name.toLowerCase() === name.toLowerCase(),
+//   //     )
+//   //   ) {
+//   //     alert(`${newContact.name} you heve this contact`);
+//   //     return;
+//   //   }
+//   //   setContacts([newContact, ...contacts]);
+//   // };
+//     const cloneContact = contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase(),
+//     if (cloneContact) {
+//       alert(`${newContact.name} you heve this contact`);
+//       return;
+//     }
+//     setContacts([newContact, ...contacts]);
+//   };
+
+//   const handleChangeFilter = e => {
+//     setFilter(e.currentTarget.value);
+//   };
+
+//   const getFilteredContacts = () => {
+//     const normalizedFilter = filter.toLowerCase();
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(normalizedFilter),
+//     );
+//   };
+
+//   // const deleteContact = contactId => {
+//   //   setContacts(prevState =>
+//   //     prevState.filter(contact => contact.id !== contactId),
+//   //   );
+//   // };
+
+//   const deleteContact = contactId => {
+//     setContacts(contacts.filter(contact => contact.id !== contactId));
+//   };
+
+//   return (
+//     <Container>
+//       <h1 className={Title}>Phonebook</h1>
+//       <ContactsForm onSubmit={addContact} />
+//       <h2 className={Title}>Contacts</h2>
+//       <Filter value={filter} onChange={handleChangeFilter} />
+//       <ContactsList
+//         contacts={getFilteredContacts()}
+//         onDeleteContact={deleteContact}
+//       />
+//     </Container>
+//   );
+// }
+
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import 'modern-normalize/modern-normalize.css';
@@ -14,21 +89,20 @@ export default function App() {
   const [contacts, setContacts] = useLocaleStorage('contacts', []);
 
   const addContact = ({ name, number }) => {
-    const nweContact = {
+    const newContact = {
       id: nanoid(),
       name,
       number,
     };
 
-    if (
-      contacts.find(
-        contact => contact.name.toLowerCase() === name.toLowerCase(),
-      )
-    ) {
-      alert(`${nweContact.name} you heve this contact`);
+    const cloneContact = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase(),
+    );
+    if (cloneContact) {
+      alert(`${newContact.name} you have this contact`);
       return;
     }
-    setContacts([nweContact, ...contacts]);
+    setContacts([newContact, ...contacts]);
   };
 
   const handleChangeFilter = e => {
@@ -43,9 +117,7 @@ export default function App() {
   };
 
   const deleteContact = contactId => {
-    setContacts(prevState =>
-      prevState.filter(contact => contact.id !== contactId),
-    );
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
   return (
